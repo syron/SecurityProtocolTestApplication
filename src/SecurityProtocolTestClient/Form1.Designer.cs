f3e602e9013bf8fbb7c56c817b07d1df3225dbfb
Robert Mayer <rwlmayer@gmail.com>
2017-01-04 13:09:52 +0100

    Initial commit

---
+++ src/SecurityProtocolTestClient/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace SecurityProtocolTestClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxSecurityProtocols = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.buttonSendRequest = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelStatusCodeValue = new System.Windows.Forms.Label();
+            this.textBoxContent = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxException = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Security Protocol";
+            // 
+            // comboBoxSecurityProtocols
+            // 
+            this.comboBoxSecurityProtocols.FormattingEnabled = true;
+            this.comboBoxSecurityProtocols.Location = new System.Drawing.Point(105, 6);
+            this.comboBoxSecurityProtocols.Name = "comboBoxSecurityProtocols";
+            this.comboBoxSecurityProtocols.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSecurityProtocols.TabIndex = 2;
+            this.comboBoxSecurityProtocols.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "URL";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(105, 35);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(355, 20);
+            this.textBoxUrl.TabIndex = 4;
+            // 
+            // buttonSendRequest
+            // 
+            this.buttonSendRequest.Location = new System.Drawing.Point(105, 62);
+            this.buttonSendRequest.Name = "buttonSendRequest";
+            this.buttonSendRequest.Size = new System.Drawing.Size(75, 23);
+            this.buttonSendRequest.TabIndex = 5;
+            this.buttonSendRequest.Text = "Send";
+            this.buttonSendRequest.UseVisualStyleBackColor = true;
+            this.buttonSendRequest.Click += new System.EventHandler(this.buttonSendRequest_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxException);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxContent);
+            this.groupBox1.Controls.Add(this.labelStatusCodeValue);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(448, 286);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Response";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Status Code";
+            // 
+            // labelStatusCodeValue
+            // 
+            this.labelStatusCodeValue.AutoSize = true;
+            this.labelStatusCodeValue.Location = new System.Drawing.Point(90, 20);
+            this.labelStatusCodeValue.Name = "labelStatusCodeValue";
+            this.labelStatusCodeValue.Size = new System.Drawing.Size(24, 13);
+            this.labelStatusCodeValue.TabIndex = 1;
+            this.labelStatusCodeValue.Text = "n/a";
+            // 
+            // textBoxContent
+            // 
+            this.textBoxContent.Location = new System.Drawing.Point(93, 36);
+            this.textBoxContent.Multiline = true;
+            this.textBoxContent.Name = "textBoxContent";
+            this.textBoxContent.Size = new System.Drawing.Size(349, 116);
+            this.textBoxContent.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Content";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Content";
+            // 
+            // textBoxException
+            // 
+            this.textBoxException.Location = new System.Drawing.Point(93, 158);
+            this.textBoxException.Multiline = true;
+            this.textBoxException.Name = "textBoxException";
+            this.textBoxException.Size = new System.Drawing.Size(349, 116);
+            this.textBoxException.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SecurityProtocolTestClient.Properties.Resources.testclient_right;
+            this.pictureBox1.Location = new System.Drawing.Point(472, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(394, 522);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 565);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonSendRequest);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxSecurityProtocols);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Security Protocol Test Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxSecurityProtocols;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.Button buttonSendRequest;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelStatusCodeValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxContent;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxException;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ src/SecurityProtocolTestClient/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecurityProtocolTestClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var securityProtocols = Enum.GetValues(typeof(SecurityProtocolType)).Cast<SecurityProtocolType>();
+            comboBoxSecurityProtocols.Items.AddRange(securityProtocols.Select(sp => sp.ToString()).ToArray());
+            pictureBox1.Image.RotateFlip((RotateFlipType.Rotate90FlipNone));
+            pictureBox1.Refresh();
+        }
+
+        private async void buttonSendRequest_Click(object sender, EventArgs e)
+        {
+            var securityProtocol = comboBoxSecurityProtocols.SelectedItem;
+            var url = textBoxUrl.Text;
+            if (securityProtocol == null || string.IsNullOrWhiteSpace(securityProtocol.ToString()))
+            {
+                MessageBox.Show("Please select a security protocol.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (string.IsNullOrWhiteSpace(url))
+            {
+                MessageBox.Show("Please provide an URL.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try { 
+                    ServicePointManager.SecurityProtocol = (SecurityProtocolType)Enum.Parse(typeof(SecurityProtocolType), securityProtocol.ToString());
+                    using (var client = new HttpClient())
+                    {
+                        var t_response = await client.GetAsync(url);
+
+                        labelStatusCodeValue.Text = $"{t_response.StatusCode.ToString()} ({(int)t_response.StatusCode})";
+
+                        var content = await t_response.Content.ReadAsStringAsync();
+                        textBoxContent.Text = content;
+                        textBoxException.Text = string.Empty;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    textBoxException.Text = ex.ToString();
+                }
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var dialogRslt = MessageBox.Show("ROBERT MAYER AWESOMENESS!!!!", "AWESOMENESS", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+
+            if (dialogRslt == DialogResult.Yes)
+            {
+
+            }
+            else if (dialogRslt == DialogResult.No)
+            {
+
+            }
+            else if (dialogRslt == DialogResult.Cancel)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
